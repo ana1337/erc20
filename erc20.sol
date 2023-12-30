3a63332b169299f0cdc9953f30ce81af709a05c7
ana1337 <143551395+ana1337@users.noreply.github.com>
2023-12-30 13:33:34 +0300

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/manager/AccessManaged.sol";
+
+contract MyToken is ERC20, AccessManaged {
+    constructor(address initialAuthority)
+        ERC20("MyToken", "MTK")
+        AccessManaged(initialAuthority)
+    {}
+
+    function mint(address to, uint256 amount) public restricted {
+        _mint(to, amount);
+    }
+}
